2e4c493e2f23e1d1b80fee929aff211364206ede
Naitik1801 <96631140+Naitik1801@users.noreply.github.com>
2022-04-16 19:48:23 +0530

    Create HourGlassPattern.py

---
+++ HourGlassPattern.py
@@ -0,0 +1,19 @@
+for i in range(5,1,-1):
+    print(" "*(5-i) + "* " * i)
+   
+for i in range(1,6):
+    print(" "*(5-i) + "* " * i)
+    
+print('\n')
+
+i = 5
+while i>1:
+ 
+   print(" "*(5-i) + "* " * i)
+   i-=1
+
+i = 1
+while i<=5:
+   
+   print(" "*(5-i) + "* " * i)
+   i+=1
